9db01aef97b1600272c92628bbc2bb98dbb2f742
Suleman <slmn95@rediffmail.com>
2018-03-08 23:50:53 +0530

    Update bot.py
    Added functionality to reply with random tweets from a text file.

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import tweepy
+import random
 #from our keys module (keys.py), import the keys dictionary
 from keys import keys
- 
+
 CONSUMER_KEY = keys['consumer_key']
 CONSUMER_SECRET = keys['consumer_secret']
 ACCESS_TOKEN = keys['access_token']
@@ -12,11 +13,11 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
  
-twt = api.search("#TVD",result_type="mixed",count=3) 
+twt = api.search("#TVD",result_type="new",count=5) 
  
 for s in twt:
    print(s.id)
    sn = s.user.screen_name
-   m = "@%s Good Morning Sunshine!" % (sn)
+   m = "@%s " %sn + random.choice(open('tweets.txt').readlines()).strip("\n") 
    api.update_status(status=m, in_reply_to_status_id = s.id)
 print("Done!!!")
