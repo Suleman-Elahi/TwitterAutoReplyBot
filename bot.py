ae51a38bfa2ed2934d61941de8521eb1ba010767
Suleman <slmn95@rediffmail.com>
2021-08-17 15:31:39 +0530

    Made it a single standalone script.

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import tweepy, random
-#from our keys module (keys.py), import the keys dictionary
-from keys import keys
+import time
 
 CONSUMER_KEY = keys['consumer_key']
 CONSUMER_SECRET = keys['consumer_secret']
@@ -15,6 +14,7 @@ api = tweepy.API(auth)
 twt = api.search("#TVD",result_type="new",count=5) 
  
 for s in twt:
+   time.sleep(3)
    print(s.id)
    sn = s.user.screen_name
    m = "@%s " %sn + random.choice(open('tweets.txt').readlines()).strip("\n") 
