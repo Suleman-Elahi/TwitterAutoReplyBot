81f6d500410d753a26ba4049edcf5dc2406f93e6
Suleman <slmn95@rediffmail.com>
2022-04-04 17:18:51 +0530

    vars change

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-import tweepy, random
-import time
+import tweepy
 import pickle
+import time, random
 
 ##########################################################################################
 CONSUMER_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"                           #
@@ -9,7 +9,7 @@ CONSUMER_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"          
 ACCESS_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"                      #
 ACCESS_TOKEN_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"                    #
 HASHTAG = "#tvd"                                                                         #
-NUMBER_OF_TWEETS_TO_RERPLY = 5                                                           #
+NUMBER_OF_TWEETS_TO_REPLY = 5                                                           #
 TWEETS_TYPE = "recent" #can be set to "mixed" or "popular" as well                       #
 ##########################################################################################
 
@@ -25,7 +25,7 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
  
-twt = api.search_tweets(HASHTAG,result_type=TWEETS_TYPE,count=NUMBER_OF_TWEETS_TO_RERPLY) 
+twt = api.search_tweets(HASHTAG,result_type=TWEETS_TYPE,count=NUMBER_OF_TWEETS_TO_REPLY)
  
 for s in twt:
    if s.id not in processed_tweets:
